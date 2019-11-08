67f5efd98def3f15b9ac671ed293124f57862088
DallinToastMEN <rodgers.dallin@gmail.com>
2019-11-07 19:06:27 -0700

    Created timer click funtion
    Placed the timer function in a click event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,33 @@
-var timeLeft = 10;
-var elem = document.querySelector(".time");
+// var timeLeft = 10;
+// var elem = document.querySelector(".time");
 
-var timerId = setInterval(countdown, 1000);
+// // var timerId = setInterval(countdown, 1000);
 
-function countdown() {
-  if (timeLeft == 0) {
-    elem.innerHTML = timeLeft;
-    clearTimeout(timerId);
-    doSomething();
-  } else {
-    elem.innerHTML = timeLeft;
-    timeLeft--;
+// function countdown() {
+//   if (timeLeft == 0) {
+//     elem.innerHTML = timeLeft;
+//     clearTimeout(timerId);
+//     doSomething();
+//   } else {
+//     elem.innerHTML = timeLeft;
+//     timeLeft--;
+//   }
+// }
+
+$(".start-game").on("click", function() {
+  var timeLeft = 10;
+  var elem = document.querySelector(".time");
+
+  var timerId = setInterval(countdown, 1000);
+
+  function countdown() {
+    if (timeLeft == 0) {
+      elem.innerHTML = timeLeft;
+      clearTimeout(timerId);
+      // doSomething();
+    } else {
+      elem.innerHTML = timeLeft;
+      timeLeft--;
+    }
   }
-}
+});
